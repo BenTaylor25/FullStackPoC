cd65b5e43d38c899d3c921abb44e5de2c563e5ec
Ben Taylor <ben.taylor2573@gmail.com>
2024-08-28 17:21:02 +0100

    push and get value components on main page

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,9 +1,27 @@
+import PushValue from '../components/PushValue';
+import GetValue from '../components/GetValue';
+
+import './MainPage.css';
 
 export default function MainPage() {
 
     return (
         <>
-            main page test
+            <main id="main-page">
+
+                <div className="grid">
+
+                    <div className="grid-row">
+                        <PushValue />
+                    </div>
+
+                    <div className="grid-row">
+                        <GetValue />
+                    </div>
+
+                </div>
+
+            </main>
         </>
     );
 }
