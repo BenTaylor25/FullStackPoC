94507f3910a6679c132d46ac1f44e8955c3139a0
Ben Taylor <ben.taylor2573@gmail.com>
2024-08-28 16:54:49 +0100

    navbar

--- frontend/src/pages/layout/MyNav.tsx
+++ frontend/src/pages/layout/MyNav.tsx
@@ -8,6 +8,18 @@ export default function MyNav() {
 
                 <span id="nav-title">Proof of Concept App</span>
 
+                <div id="nav-control">
+
+                    <a>
+                        <span>Page 1</span>
+                    </a>
+
+                    <a>
+                        <span>Page 2</span>
+                    </a>
+
+                </div>
+
             </nav>
         </>
     );
